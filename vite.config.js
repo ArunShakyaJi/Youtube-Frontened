d5575b69a4c914c8476100a9b7f4d732d261a797
ArunShakyaJi <shakyaarun173@gmail.com>
2024-05-10 17:29:57 +0530

    chore: Remove unused proxy configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:8000/api/v1'
-    }
-  }
+  
 })
