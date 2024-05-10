27b2dd1eb51aef7b76858a8ff733973dd0013fa7
ArunShakyaJi <shakyaarun173@gmail.com>
2024-05-10 10:06:32 +0530

    chore: Update proxy URL in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:8000/api/v1'
+      '/api': 'youtube-twitter-ten.vercel.app'
     }
   }
 })
